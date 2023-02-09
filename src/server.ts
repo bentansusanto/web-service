7a84c115851d4900124be2227f8275c10165bb7d
bentansusanto <tansusanto194@gmail.com>
2023-02-09 17:26:35 +0700

    create customer api

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { Application } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import {router} from './src/routes.ts'
+import {router} from './routes.ts';
 const app = new Application();
 
 // Logger
